b0985eb26a1f57627cc2fb3905b0baabe10763f0
Juve2409 <bustamantejuve2409@gmail.com>
2026-02-10 11:00:35 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+export default function App() {
+  const fecha = new Date().toLocaleString();
+
+  return (
+    <main>
+      <h1>Hola Somos Aprendices SENA ❤️😜 </h1>
+      <p>En este curso de React esperamos aprender a crear interfaces modernas, manejar componentes, estados y construir páginas web dinámicas✨</p>
+      <p>Fecha y hora actual: {fecha}</p>
+
+    </main>
+  );
+}
+
